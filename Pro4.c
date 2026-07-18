048301752fbeeb71687acbb0bbed799afb77dd66
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-07-19 02:42:34 +0530

    Dijkstra's algorithm implementation

--- Pro4.c
+++ Pro4.c
@@ -1,57 +1,64 @@
-#include <stdio.h>
-#include <limits.h>
-#include <stdbool.h>
+#include<stdio.h>
 
-#define MAX 10
-#define INF INT_MAX
+#define INF 999
 
-int minDistance(int dist[], bool sptSet[], int V) {
-    int min = INF, idx = -1;
-    for (int i = 0; i < V; i++)
-        if (!sptSet[i] && dist[i] <= min)
-            min = dist[i], idx = i;
-    return idx;
+void dijkstra(int c[10][10], int n, int s, int d[10])
+{
+int v[10], min, u, i, j;
+
+for(i=1; i<=n; i++)
+{
+d[i]=c[s][i];
+v[i]=0;
 }
 
-void dijkstra(int graph[MAX][MAX], int src, int V) {
-    int dist[MAX];
-    bool sptSet[MAX] = {0};
+v[s]=1;
 
-    for (int i = 0; i < V; i++)
-        dist[i] = INF;
+for(i=1; i<=n; i++)
+{
+min=INF;
 
-    dist[src] = 0;
-
-    for (int count = 0; count < V - 1; count++) {
-        int u = minDistance(dist, sptSet, V);
-        sptSet[u] = true;
-
-        for (int v = 0; v < V; v++)
-            if (!sptSet[v] && graph[u][v] && dist[u] != INF &&
-                dist[u] + graph[u][v] < dist[v])
-                dist[v] = dist[u] + graph[u][v];
-    }
-
-    printf("Vertex \t\t Distance from Source\n");
-    for (int i = 0; i < V; i++)
-        printf("%d \t\t %d\n", i, dist[i]);
+for(j=1; j<=n; j++)
+{
+if(v[j]==0 && d[j]<min)
+{
+min=d[j];
+u=j;
+}
 }
 
-int main() {
-    int V, E, graph[MAX][MAX] = {0};
-    printf("Enter the number of vertices: ");
-    scanf("%d", &V);
-    printf("Enter the number of edges: ");
-    scanf("%d", &E);
+v[u]=1;
 
-    printf("Enter the source vertex, destination vertex, and weight for each edge:\n");
-    for (int i = 0; i < E; i++) {
-        int s, d, w;
-        scanf("%d %d %d", &s, &d, &w);
-        graph[s][d] = w;
-        graph[d][s] = w;
-    }
+for(j=1; j<=n; j++)
+{
+if(v[j]==0 && (d[u]+c[u][j])<d[j])
+d[j]=d[u]+c[u][j];
+}
+}
+}
 
-    dijkstra(graph, 0, V);
-    return 0;
+int main()
+{
+int c[10][10], d[10], i, j, s, sum, n;
+
+printf("Enter n value: ");
+scanf("%d",&n);
+
+printf("Enter the graph data:\n");
+
+for(i=1; i<=n; i++)
+for(j=1; j<=n; j++)
+scanf("%d",&c[i][j]);
+
+printf("Enter the source node: ");
+scanf("%d",&s);
+
+dijkstra(c,n,s,d);
+
+printf("\nShortest distances from source vertex %d:\n",s);
+
+for(i=1; i<=n; i++)
+printf("Shortest distance from %d to %d is %d\n",s,i,d[i]);
+
+return 0;
 }
