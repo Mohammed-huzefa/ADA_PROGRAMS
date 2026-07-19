b985f19a23d6ce21fd567e5e6d4c6454430cd5d7
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-07-20 04:41:56 +0530

    Update Pro2.c

--- Pro2.c
+++ Pro2.c
@@ -1,61 +1,39 @@
 #include <stdio.h>
-#include <limits.h>
-
-#define MAX 100
-
-int minKey(int key[], int visited[], int V) {
-    int min = INT_MAX, index = -1;
-    for (int i = 0; i < V; i++) {
-        if (!visited[i] && key[i] < min) {
-            min = key[i];
-            index = i;
-        }
-    }
-    return index;
-}
-
-void prim(int graph[MAX][MAX], int V) {
-    int parent[MAX], key[MAX], visited[MAX];
-
-    for (int i = 0; i < V; i++) {
-        key[i] = INT_MAX;
-        visited[i] = 0;
-    }
-
-    key[0] = 0;
-    parent[0] = -1;
-
-    for (int count = 0; count < V - 1; count++) {
-        int u = minKey(key, visited, V);
-        visited[u] = 1;
-
-        for (int v = 0; v < V; v++) {
-            if (graph[u][v] && !visited[v] && graph[u][v] < key[v]) {
-                key[v] = graph[u][v];
-                parent[v] = u;
-            }
-        }
-    }
-
-    printf("Edge      Weight\n");
-    for (int i = 1; i < V; i++)
-        printf("%d - %d      %d\n", parent[i], i, graph[i][parent[i]]);
-}
+#define INF 999
+#define MAX 20
 
 int main() {
-    int V, E, graph[MAX][MAX] = {0};
-
-    printf("Enter the number of vertices and edges: ");
-    scanf("%d %d", &V, &E);
-
-    printf("Enter the source vertex, destination vertex, and weight for each edge:\n");
-    for (int i = 0; i < E; i++) {
-        int s, d, w;
-        scanf("%d %d %d", &s, &d, &w);
-        graph[s][d] = w;
-        graph[d][s] = w;
-    }
-
-    prim(graph, V);
-    return 0;
+ int n, cost[MAX][MAX], visited[MAX] = {0};
+ int i, j, source, edges = 1, min_cost = 0;
+    
+ printf("Enter the no. of nodes:");
+ scanf("%d", &n);
+ printf("Enter the cost matrix:\n");
+ for (i = 1; i <= n; i++)
+ for (j = 1; j <= n; j++)
+ scanf("%d", &cost[i][j]);
+    
+ printf("Enter the root node:");
+ scanf("%d", &source);
+ visited[source] = 1;
+    
+ printf("\nMinimum cost spanning tree is\n");
+ while (edges < n) {
+ int min = INF, a = 0, b = 0;
+ for (i = 1; i <= n; i++)
+ if (visited[i])
+ for (j = 1; j <= n; j++)
+ if (!visited[j] && cost[i][j] < min) {
+ min = cost[i][j];
+ a = i;
+ b = j;
+ }
+     
+ printf("\nEdge %d\t(%d->%d)=%d\n", edges++, a, b, min);
+ visited[b] = 1;
+ min_cost += min;
+ }
+    
+ printf("\nMinimum cost=%d\n", min_cost);
+ return 0;
 }
